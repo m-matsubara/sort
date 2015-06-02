cec6d00f7dc2a998b640c7d30ba2ab3ac61278cf
masakazu matsubara <matsu.no.page@gmail.com>
2015-06-03 07:57:02 +0900

    コマンドプロンプトのタイトルを随時変更してどこまで実行されているかわかるようにした

--- src/test.bat
+++ src/test.bat
@@ -4,32 +4,59 @@ rem Released under the MIT license
 rem https://github.com/m-matsubara/sort/blob/master/LICENSE.txt
 
 set TIMES=%2
-
+title %1 100 R
 java.exe  mmsort.SortTest %1 100 R %TIMES%
+title %1 100 A
 java.exe  mmsort.SortTest %1 100 A %TIMES%
+title %1 100 D
 java.exe  mmsort.SortTest %1 100 D %TIMES%
+title %1 100 F
 java.exe  mmsort.SortTest %1 100 F %TIMES%
+title %1 1000 R
 java.exe  mmsort.SortTest %1 1000 R %TIMES%
+title %1 1000 A
 java.exe  mmsort.SortTest %1 1000 A %TIMES%
+title %1 1000 D
 java.exe  mmsort.SortTest %1 1000 D %TIMES%
+title %1 1000 F
 java.exe  mmsort.SortTest %1 1000 F %TIMES%
+title %1 10000 R
 java.exe  mmsort.SortTest %1 10000 R %TIMES%
+title %1 10000 A
 java.exe  mmsort.SortTest %1 10000 A %TIMES%
+title %1 10000 D
 java.exe  mmsort.SortTest %1 10000 D %TIMES%
+title %1 10000 F
 java.exe  mmsort.SortTest %1 10000 F %TIMES%
+title %1 100000 R
 java.exe  mmsort.SortTest %1 100000 R %TIMES%
+title %1 100000 A
 java.exe  mmsort.SortTest %1 100000 A %TIMES%
+title %1 100000 D
 java.exe  mmsort.SortTest %1 100000 D %TIMES%
+title %1 100000 F
 java.exe  mmsort.SortTest %1 100000 F %TIMES%
+title %1 1000000 R
 java.exe  mmsort.SortTest %1 1000000 R %TIMES%
+title %1 1000000 A
 java.exe  mmsort.SortTest %1 1000000 A %TIMES%
+title %1 1000000 D
 java.exe  mmsort.SortTest %1 1000000 D %TIMES%
+title %1 1000000 F
 java.exe  mmsort.SortTest %1 1000000 F %TIMES%
+title %1 10000000 R
 java.exe  mmsort.SortTest %1 10000000 R %TIMES%
+title %1 10000000 A
 java.exe  mmsort.SortTest %1 10000000 A %TIMES%
+title %1 10000000 D
 java.exe  mmsort.SortTest %1 10000000 D %TIMES%
+title %1 10000000 F
 java.exe  mmsort.SortTest %1 10000000 F %TIMES%
+title %1 100000000 R
 java.exe  mmsort.SortTest %1 100000000 R %TIMES%
+title %1 100000000 A
 java.exe  mmsort.SortTest %1 100000000 A %TIMES%
+title %1 100000000 D
 java.exe  mmsort.SortTest %1 100000000 D %TIMES%
+title %1 100000000 F
 java.exe  mmsort.SortTest %1 100000000 F %TIMES%
