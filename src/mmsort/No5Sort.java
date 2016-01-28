925ad9b85b78431fc5dc63332c4c6df56dcdef5d
matsubara <matsubara@Empress>
2016-01-28 23:52:30 +0900

    No5Sortを追加。いつか正式の名前付ける。

---
+++ src/mmsort/No5Sort.java
@@ -0,0 +1,237 @@
+/*
+ * Quick sort (Median of 3)
+ *
+ * Copyright (c) 2015 masakazu matsubara
+ * Released under the MIT license
+ * https://github.com/m-matsubara/sort/blob/master/LICENSE.txt
+ */
+package mmsort;
+
+import java.util.Comparator;
+
+public class No5Sort implements ISortAlgorithm {
+
+	public static final <T> void sort5(final T[] array, int p1, int p2, int p3, int p4, int p5, final Comparator<? super T> comparator)
+	{
+		T v1 = array[p1];
+		T v2 = array[p2];
+		T v3 = array[p3];
+		T v4 = array[p4];
+		T v5 = array[p5];
+
+		//	まず、先頭３つのソート
+		if (comparator.compare(v1, v2) <= 0) {
+			if (comparator.compare(v2, v3) <= 0) {
+				// v1 <= v2 <= v3
+				//array[p1] = v1;
+				//array[p2] = v2;
+				//array[p3] = v3;
+			} else if (comparator.compare(v1, v3) <= 0) {
+				// v1 <= v3 <= v2
+				//array[p1] = v1;
+				array[p2] = v3;
+				array[p3] = v2;
+			} else {
+				// v3 <= v1 <= v2
+				array[p1] = v3;
+				array[p2] = v1;
+				array[p3] = v2;
+			}
+		} else {
+			if (comparator.compare(v1, v3) <= 0) {
+				// v2 <= v1 <= v3
+				array[p1] = v2;
+				array[p2] = v1;
+				//array[p3] = v3;
+			} else if (comparator.compare(v2, v3) <= 0) {
+				// v2 <= v3 <= v1
+				array[p1] = v2;
+				array[p2] = v3;
+				array[p3] = v1;
+			} else {
+				// v3 <= v2 <= v1
+				array[p1] = v3;
+				//array[p2] = v2;
+				array[p3] = v1;
+			}
+		}
+
+		// v4 ( = array[p4]) を挿入ソートっぽく指定位置に挿入
+		if (comparator.compare(array[p2], v4) <= 0) {
+			if (comparator.compare(array[p3], v4) <= 0) {
+				// array[p3] <= v4
+			} else {
+				// array[p2] <= v4 < array[p3];
+				array[p4] = array[p3];
+				array[p3] = v4;
+			}
+		} else {
+			if (comparator.compare(array[p1], v4) <= 0) {
+				// array[p1] <= v4 < array[p2];
+				array[p4] = array[p3];
+				array[p3] = array[p2];
+				array[p2] = v4;
+			} else {
+				// v4 < array[p1] <= array[p2];
+				array[p4] = array[p3];
+				array[p3] = array[p2];
+				array[p2] = array[p1];
+				array[p1] = v4;
+			}
+		}
+
+		// v5 ( = array[p5]) を挿入ソートっぽく指定位置に挿入
+		if (comparator.compare(array[p3], v5) <= 0) {
+			// array[p3] <= v5
+			if (comparator.compare(array[p4], v5) <= 0) {
+				// array[p3] <= array[4] <= v5
+			} else {
+				// array[p3] <= v5 < array[p4]
+				array[p5] = array[p4];
+				array[p4] = v5;
+			}
+		} else {
+			// v5 < array[p3]
+			if (comparator.compare(array[p2], v5) <= 0) {
+				// array[p2] <= v5 < array[p3]
+				array[p5] = array[p4];
+				array[p4] = array[p3];
+				array[p3] = v5;
+			} else {
+				// v5 < array[p2] <= array[p3]
+				if (comparator.compare(array[p1], v5) <= 0) {
+					// array[p1] <= v5 < array[p2] <= array[p3]
+					array[p5] = array[p4];
+					array[p4] = array[p3];
+					array[p3] = array[p2];
+					array[p2] = v5;
+				} else {
+					// v5 < array[p1] <= array[p2] <= array[p3]
+					array[p5] = array[p4];
+					array[p4] = array[p3];
+					array[p3] = array[p2];
+					array[p2] = array[p1];
+					array[p1] = v5;
+				}
+			}
+		}
+	}
+
+	/**
+	 * No5 Sort
+	 *
+	 * @param array sort target / ソート対象
+	 * @param from index of first element / ソート対象の開始位置
+	 * @param to index of last element (exclusive) / ソート対象の終了位置 + 1
+	 * @param comparator comparator of array element / 比較器
+	 */
+	public static final <T> void no5Sort(final T[] array, final int from, final int to, final Comparator<? super T> comparator)
+	{
+		final int range = to - from;		//	ソート範囲サイズ
+
+		//	ソート対象配列サイズが３以下のときは特別扱い
+		if (range <= 1) {
+			return;
+		} else if (range == 2) {
+			if (comparator.compare(array[from + 1], array[from]) < 0) {
+				T work = array[from];
+				array[from] = array[from + 1];
+				array[from + 1] = work;
+			}
+			return;
+		} else if (range == 3) {
+			if (comparator.compare(array[from + 1], array[from]) < 0) {
+				T work = array[from];
+				array[from] = array[from + 1];
+				array[from + 1] = work;
+			}
+			if (comparator.compare(array[from + 2], array[from + 1]) < 0) {
+				T work = array[from + 1];
+				array[from + 1] = array[from + 2];
+				array[from + 2] = work;
+				if (comparator.compare(array[from + 1], array[from]) < 0) {
+					work = array[from];
+					array[from] = array[from + 1];
+					array[from + 1] = work;
+				}
+			}
+			return;
+		}
+
+		if (range < 10) {
+			//InsertionSort.insertionSort(array, from, to, comparator);
+			BinInsertionSort.binInsertionSort(array, from, to, comparator);
+			return;
+		}
+
+		int p1 = from;
+		int p5 = to - 1;
+		int p3 = p1 + (p5 - p1) / 2;
+		int p2 = p1 + (p3 - p1) / 2;
+		int p4 = p3 + (p5 - p3) / 2;
+
+		sort5(array, p1, p2, p3, p4, p5, comparator);
+
+		final T pivot = array[p3];	//	ピボット値
+
+		T work = array[from + 1];
+		array[from + 1] = array[p2];
+		array[p2] = work;
+
+		work = array[from + 2];
+		array[from + 2] = array[p3];
+		array[p3] = work;
+
+		work = array[to - 2];
+		array[to - 2] = array[p4];
+		array[p4] = work;
+
+		int curFrom = from + 3;			//	min index / 現在処理中位置の小さい方の位置
+		int curTo = to - 1 - 2;			//	max index / 現在処理中位置の大きい方の位置
+
+		do {
+			while (comparator.compare(array[curFrom], pivot) < 0) {
+				curFrom++;
+			}
+			while (comparator.compare(pivot, array[curTo]) < 0) {
+				curTo--;
+			}
+			if (curFrom <= curTo) {
+				work = array[curFrom];
+				array[curFrom] = array[curTo];
+				array[curTo] = work;
+				curFrom++;
+				curTo--;
+			}
+		} while (curFrom <= curTo);
+
+		array[from + 2] = array[curTo];
+		array[curTo] = pivot;
+
+		if (from < curTo) {
+			no5Sort(array, from, curTo, comparator);
+		}
+
+		if (curFrom < to - 1) {
+			no5Sort(array, curFrom, to, comparator);
+		}
+	}
+
+	@Override
+	public <T> void sort(final T[] array, final int from, final int to, final Comparator<? super T> comparator)
+	{
+		no5Sort(array, from, to, comparator);
+	}
+
+	@Override
+	public boolean isStable()
+	{
+		return false;
+	}
+
+	@Override
+	public String getName()
+	{
+		return "No5 Sort";
+	}
+}
