3f03e27b270e9bc8a2eaf8d3e1916f6208aecf86
matsubara <matsubara@Empress>
2016-05-22 22:57:08 +0900

    不要ロジック削除

--- src/mmsort/No6Sort.java
+++ src/mmsort/No6Sort.java
@@ -14,125 +14,6 @@ package mmsort;
 import java.util.Comparator;
 
 public class No6Sort implements ISortAlgorithm {
-
-	/**
-	 * 配列中の任意の５か所をソートする
-	 * （３番目の要素からピボット値を得るために使用）
-	 * @param array sort target / ソート対象
-	 * @param p1 index 1 / 添え字1
-	 * @param p2 index 2 / 添え字2
-	 * @param p3 index 3 / 添え字3
-	 * @param p4 index 4 / 添え字4
-	 * @param p5 index 5 / 添え字5
-	 * @param comparator
-	 */
-	@SuppressWarnings("unused")
-	private static final <T> void sort5(final T[] array, final int p1, final int p2, final int p3, final int p4, final int p5, final Comparator<? super T> comparator)
-	{
-		final T v1 = array[p1];
-		final T v2 = array[p2];
-		final T v3 = array[p3];
-		final T v4 = array[p4];
-		final T v5 = array[p5];
-
-		//	まず、先頭３つのソート
-		if (comparator.compare(v1, v2) <= 0) {
-			if (comparator.compare(v2, v3) <= 0) {
-				// v1 <= v2 <= v3
-				//array[p1] = v1;
-				//array[p2] = v2;
-				//array[p3] = v3;
-			} else if (comparator.compare(v1, v3) <= 0) {
-				// v1 <= v3 <= v2
-				//array[p1] = v1;
-				array[p2] = v3;
-				array[p3] = v2;
-			} else {
-				// v3 <= v1 <= v2
-				array[p1] = v3;
-				array[p2] = v1;
-				array[p3] = v2;
-			}
-		} else {
-			if (comparator.compare(v1, v3) <= 0) {
-				// v2 <= v1 <= v3
-				array[p1] = v2;
-				array[p2] = v1;
-				//array[p3] = v3;
-			} else if (comparator.compare(v2, v3) <= 0) {
-				// v2 <= v3 <= v1
-				array[p1] = v2;
-				array[p2] = v3;
-				array[p3] = v1;
-			} else {
-				// v3 <= v2 <= v1
-				array[p1] = v3;
-				//array[p2] = v2;
-				array[p3] = v1;
-			}
-		}
-
-		// v4 ( = array[p4]) を挿入ソートっぽく指定位置に挿入
-		if (comparator.compare(array[p2], v4) <= 0) {
-			if (comparator.compare(array[p3], v4) <= 0) {
-				// array[p3] <= v4
-			} else {
-				// array[p2] <= v4 < array[p3];
-				array[p4] = array[p3];
-				array[p3] = v4;
-			}
-		} else {
-			if (comparator.compare(array[p1], v4) <= 0) {
-				// array[p1] <= v4 < array[p2];
-				array[p4] = array[p3];
-				array[p3] = array[p2];
-				array[p2] = v4;
-			} else {
-				// v4 < array[p1] <= array[p2];
-				array[p4] = array[p3];
-				array[p3] = array[p2];
-				array[p2] = array[p1];
-				array[p1] = v4;
-			}
-		}
-
-		// v5 ( = array[p5]) を挿入ソートっぽく指定位置に挿入
-		if (comparator.compare(array[p3], v5) <= 0) {
-			// array[p3] <= v5
-			if (comparator.compare(array[p4], v5) <= 0) {
-				// array[p3] <= array[4] <= v5
-			} else {
-				// array[p3] <= v5 < array[p4]
-				array[p5] = array[p4];
-				array[p4] = v5;
-			}
-		} else {
-			// v5 < array[p3]
-			if (comparator.compare(array[p2], v5) <= 0) {
-				// array[p2] <= v5 < array[p3]
-				array[p5] = array[p4];
-				array[p4] = array[p3];
-				array[p3] = v5;
-			} else {
-				// v5 < array[p2] <= array[p3]
-				if (comparator.compare(array[p1], v5) <= 0) {
-					// array[p1] <= v5 < array[p2] <= array[p3]
-					array[p5] = array[p4];
-					array[p4] = array[p3];
-					array[p3] = array[p2];
-					array[p2] = v5;
-				} else {
-					// v5 < array[p1] <= array[p2] <= array[p3]
-					array[p5] = array[p4];
-					array[p4] = array[p3];
-					array[p3] = array[p2];
-					array[p2] = array[p1];
-					array[p1] = v5;
-				}
-			}
-		}
-	}
-
 	/**
 	 * No6 Sort
 	 *
@@ -171,7 +52,7 @@ public class No6Sort implements ISortAlgorithm {
 		final int p1 = p2 - gap;
 		final int p4 = p3 + gap;
 		final int p5 = p4 + gap;
-		//sort5(array, p1, p2, p3, p4, p5, comparator);
+
 		T v1 = array[p1];
 		T v2 = array[p2];
 		T v3 = array[p3];
@@ -303,10 +184,10 @@ public class No6Sort implements ISortAlgorithm {
 				T value = array[idx];
 				if (comparator.compare(value, pivot1) <= 0) {
 					array[idx1A++] = value;		// TODO ここで余計な代入が発生するケースがあるが、下手に最適化すると逆に遅くなってしまう…。
-				} else if (comparator.compare(value, pivot2) < 0) {
-					works[idx2W++] = value;
-				} else {
+				} else if (comparator.compare(value, pivot2) >= 0) {
 					works[idx3W--] = value;
+				} else {
+					works[idx2W++] = value;
 				}
 			}
 			int idxTo = idx1A;
