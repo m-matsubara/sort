6dc42202ff44b7a92d2d48cd35c7366c99e7681d
matsubara <matsubara@Empress>
2016-05-22 10:06:44 +0900

    遅いソートアルゴリズムも追加（ただし、コメントアウト）

--- testAll.bat
+++ testAll.bat
@@ -4,6 +4,7 @@ rem Released under the MIT license
 rem https://github.com/m-matsubara/sort/blob/master/LICENSE.txt
 
 rem unstable sort
+rem call test.bat mmsort.CombSort %1
 call test.bat mmsort.ManyPivotSort %1
 call test.bat mmsort.ManyPivotSort3W %1
 call test.bat mmsort.No5Sort %1
@@ -12,11 +13,14 @@ call test.bat mmsort.QuickSort3WM5 %1
 call test.bat mmsort.QuickSortM3 %1
 call test.bat mmsort.QuickSortM5 %1
 
+
 rem stable sort
 call test.bat mmsort.ArraysSort %1
+rem call test.bat mmsort.BinInsersionSort %1
 call test.bat mmsort.ImprovedMergeSort %1
+rem call test.bat mmsort.InsersionSort %1
 call test.bat mmsort.InplaceMergeSort %1
-call test.bat mmsort.No6Sort %1
 call test.bat mmsort.MasSort %1
 call test.bat mmsort.MatSort %1
 call test.bat mmsort.MergeSort %1
+call test.bat mmsort.No6Sort %1
