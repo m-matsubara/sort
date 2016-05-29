9a9ba57dfdef40f6eae2a31ff153a4a4425cce45
matsubara <matsubara@Empress>
2016-05-29 15:20:28 +0900

    重複なし乱数データに対応

--- sql/query.sql
+++ sql/query.sql
@@ -22,6 +22,30 @@ order by
   , SR.N100
 ;
 
+--*DataTitle 重複なし乱数データ(実行時間)
+select 
+  SR.ALGORITHM as "アルゴリズム"
+  , case when SR.STABLE = 'stable' then '安定' else ' ' end as "安定"
+  , SR.N100 as "100"
+  , SR.N1000 as "1000"
+  , SR.N10000 as "10000"
+  , SR.N100000 as "100000"
+  , SR.N1000000 as "1000000"
+  , SR.N10000000 as "10000000"
+  /*, SR.N100000000 as "100000000"*/
+from
+  VW_SORT_REPORT_TIME SR
+where
+  SR.ARRAY_TYPE = 'Unique Random'
+order by 
+  SR.N10000000
+  , SR.N1000000
+  , SR.N100000
+  , SR.N10000
+  , SR.N1000
+  , SR.N100
+;
+
 --*DataTitle 前半ソート済み・後半乱数(実行時間)
 select 
   SR.ALGORITHM as "アルゴリズム"
@@ -133,6 +157,30 @@ from
   VW_SORT_REPORT_COMPARE SR
 where
   SR.ARRAY_TYPE = 'Random'
+order by 
+  SR.N10000000
+  , SR.N1000000
+  , SR.N100000
+  , SR.N10000
+  , SR.N1000
+  , SR.N100
+;
+
+--*DataTitle 重複なし乱数データ(比較回数)
+select 
+  SR.ALGORITHM as "アルゴリズム"
+  , case when SR.STABLE = 'stable' then '安定' else ' ' end as "安定"
+  , SR.N100 as "100"
+  , SR.N1000 as "1000"
+  , SR.N10000 as "10000"
+  , SR.N100000 as "100000"
+  , SR.N1000000 as "1000000"
+  , SR.N10000000 as "10000000"
+  /*, SR.N100000000 as "100000000"*/
+from
+  VW_SORT_REPORT_COMPARE SR
+where
+  SR.ARRAY_TYPE = 'Unique Random'
 order by 
   SR.N10000000
   , SR.N1000000
