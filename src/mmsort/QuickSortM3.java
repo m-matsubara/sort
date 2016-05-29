e7c24688cca2b341b88793223b77ac827dccac06
matsubara <matsubara@Empress>
2016-05-29 10:12:56 +0900

    定数値を追加

--- src/mmsort/QuickSortM3.java
+++ src/mmsort/QuickSortM3.java
@@ -12,6 +12,9 @@ package mmsort;
 import java.util.Comparator;
 
 public class QuickSortM3 implements ISortAlgorithm {
+	// Insersion Sortなどに切り替える要素数
+	public static final int ALGORITHM_THRESHOLD = 20;
+
 	/**
 	 * Quick sort (Median of 3)
 	 * クイックソート（３つのメディアン）
@@ -25,9 +28,9 @@ public class QuickSortM3 implements ISortAlgorithm {
 		final int range = to - from;		//	ソート範囲サイズ
 
 		//	ソート対象配列サイズが一定数以下のときは特別扱い
-		if (range < 20) {
-			//InsertionSort.insertionSort(array, from, to, comparator);
-			BinInsertionSort.binInsertionSort(array, from, to, comparator);
+		if (range < ALGORITHM_THRESHOLD) {
+			InsertionSort.insertionSort(array, from, to, comparator);
+			//BinInsertionSort.binInsertionSort(array, from, to, comparator);
 			return;
 		}
 
