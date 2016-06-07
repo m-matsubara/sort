78f9783859c6b56f44358507d1965ee2f657cc29
matsubara <matsubara@Empress>
2016-06-07 23:31:28 +0900

    要素数が１以下のときの不具合修正

--- src/mmsort/QuickSort.java
+++ src/mmsort/QuickSort.java
@@ -4,7 +4,7 @@
  *
  * http://www.mmatsubara.com/developer/sort/
  *
- * Copyright (c) 2015 masakazu matsubara
+ * Copyright (c) 2015 matsubara masakazu
  * Released under the MIT license
  * https://github.com/m-matsubara/sort/blob/master/LICENSE.txt
  */
@@ -27,35 +27,30 @@ public class QuickSort implements ISortAlgorithm {
 	public static final <T> void quickSort(final T[] array, final int from, final int to, final Comparator<? super T> comparator)
 	{
 		final int range = to - from;		//	ソート範囲サイズ
+		if (range < 2)
+			return;
 
 		final T pivot = array[from + range / 2];		//	ピボット値（ソート対象の中央位置）
 
 		int curFrom = from;			//	現在処理中位置の小さい方の位置
 		int curTo = to - 1;			//	現在処理中位置の大きい方の位置
 
-		do {
-			while (comparator.compare(array[curFrom], pivot) < 0) {
+		for (;;) {
+			while (comparator.compare(array[curFrom], pivot) < 0)
 				curFrom++;
-			}
-			while (comparator.compare(pivot, array[curTo]) < 0) {
+			while (comparator.compare(pivot, array[curTo]) < 0)
 				curTo--;
-			}
-			if (curFrom <= curTo) {
-				final T work = array[curFrom];
-				array[curFrom] = array[curTo];
-				array[curTo] = work;
-				curFrom++;
-				curTo--;
-			}
-		} while (curFrom <= curTo);
+			if (curFrom > curTo)
+				break;
+			final T work = array[curFrom];
+			array[curFrom] = array[curTo];
+			array[curTo] = work;
+			curFrom++;
+			curTo--;
+		};
 
-		if (from < curTo + 1) {
-			quickSort(array, from, curTo + 1, comparator);
-		}
-
-		if (curFrom < to - 1) {
-			quickSort(array, curFrom, to, comparator);
-		}
+		quickSort(array, from, curTo + 1, comparator);
+		quickSort(array, curFrom, to, comparator);
 	}
 
 	@Override
