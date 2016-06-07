5b706518e02672141619d7c408cfeb78937736e2
matsubara <matsubara@Empress>
2016-06-07 23:39:35 +0900

    実行時間の桁数変更とか。(マイクロ秒までいる？)

--- sql/ddl_oracle.sql
+++ sql/ddl_oracle.sql
@@ -1,41 +1,41 @@
---drop table TB_SORT_RESULT;
-create table TB_SORT_RESULT ( 
+drop table TB_SORT_RESULT;
+create table TB_SORT_RESULT (
   NO NUMBER(5, 0) NOT NULL ENABLE
   , ALGORITHM VARCHAR2(50)
   , ARRAY_TYPE VARCHAR2(50)
   , ARRAY_SIZE NUMBER
-  , TIME_SEC NUMBER(8, 3)
+  , TIME_SEC NUMBER(11, 6)
   , COMPARE_COUNT NUMBER
   , STABLE VARCHAR2(20)
   , PRIMARY KEY (NO, ALGORITHM, ARRAY_TYPE, ARRAY_SIZE)
-); 
+);
 
-create or replace view VW_SORT_REPORT_TIME as 
-select 
+create or replace view VW_SORT_REPORT_TIME as
+select
   SR.ARRAY_TYPE
   , SR.ALGORITHM
-  , round(avg(case SR.ARRAY_SIZE when       100 then SR.TIME_SEC else null end), 3) as "N100"
-  , round(avg(case SR.ARRAY_SIZE when      1000 then SR.TIME_SEC else null end), 3) as "N1000"
-  , round(avg(case SR.ARRAY_SIZE when     10000 then SR.TIME_SEC else null end), 3) as "N10000"
-  , round(avg(case SR.ARRAY_SIZE when    100000 then SR.TIME_SEC else null end), 3) as "N100000"
-  , round(avg(case SR.ARRAY_SIZE when   1000000 then SR.TIME_SEC else null end), 3) as "N1000000"
-  , round(avg(case SR.ARRAY_SIZE when  10000000 then SR.TIME_SEC else null end), 3) as "N10000000"
-  , round(avg(case SR.ARRAY_SIZE when 100000000 then SR.TIME_SEC else null end), 3) as "N100000000"
+  , round(avg(case SR.ARRAY_SIZE when       100 then SR.TIME_SEC else null end), 6) as "N100"
+  , round(avg(case SR.ARRAY_SIZE when      1000 then SR.TIME_SEC else null end), 6) as "N1000"
+  , round(avg(case SR.ARRAY_SIZE when     10000 then SR.TIME_SEC else null end), 6) as "N10000"
+  , round(avg(case SR.ARRAY_SIZE when    100000 then SR.TIME_SEC else null end), 6) as "N100000"
+  , round(avg(case SR.ARRAY_SIZE when   1000000 then SR.TIME_SEC else null end), 6) as "N1000000"
+  , round(avg(case SR.ARRAY_SIZE when  10000000 then SR.TIME_SEC else null end), 6) as "N10000000"
+  , round(avg(case SR.ARRAY_SIZE when 100000000 then SR.TIME_SEC else null end), 6) as "N100000000"
   , min(SR.STABLE) as STABLE
 from TB_SORT_RESULT SR
 group by SR.ARRAY_TYPE, SR.ALGORITHM;
 
-create or replace view VW_SORT_REPORT_COMPARE as 
-select 
+create or replace view VW_SORT_REPORT_COMPARE as
+select
   SR.ARRAY_TYPE
   , SR.ALGORITHM
-  , round(avg(case SR.ARRAY_SIZE when       100 then SR.COMPARE_COUNT else null end), 3) as "N100"
-  , round(avg(case SR.ARRAY_SIZE when      1000 then SR.COMPARE_COUNT else null end), 3) as "N1000"
-  , round(avg(case SR.ARRAY_SIZE when     10000 then SR.COMPARE_COUNT else null end), 3) as "N10000"
-  , round(avg(case SR.ARRAY_SIZE when    100000 then SR.COMPARE_COUNT else null end), 3) as "N100000"
-  , round(avg(case SR.ARRAY_SIZE when   1000000 then SR.COMPARE_COUNT else null end), 3) as "N1000000"
-  , round(avg(case SR.ARRAY_SIZE when  10000000 then SR.COMPARE_COUNT else null end), 3) as "N10000000"
-  , round(avg(case SR.ARRAY_SIZE when 100000000 then SR.COMPARE_COUNT else null end), 3) as "N100000000"
+  , round(avg(case SR.ARRAY_SIZE when       100 then SR.COMPARE_COUNT else null end), 1) as "N100"
+  , round(avg(case SR.ARRAY_SIZE when      1000 then SR.COMPARE_COUNT else null end), 1) as "N1000"
+  , round(avg(case SR.ARRAY_SIZE when     10000 then SR.COMPARE_COUNT else null end), 1) as "N10000"
+  , round(avg(case SR.ARRAY_SIZE when    100000 then SR.COMPARE_COUNT else null end), 1) as "N100000"
+  , round(avg(case SR.ARRAY_SIZE when   1000000 then SR.COMPARE_COUNT else null end), 1) as "N1000000"
+  , round(avg(case SR.ARRAY_SIZE when  10000000 then SR.COMPARE_COUNT else null end), 1) as "N10000000"
+  , round(avg(case SR.ARRAY_SIZE when 100000000 then SR.COMPARE_COUNT else null end), 1) as "N100000000"
   , min(SR.STABLE) as STABLE
 from TB_SORT_RESULT SR
 group by SR.ARRAY_TYPE, SR.ALGORITHM;
