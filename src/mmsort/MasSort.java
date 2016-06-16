3d3e6d690f2374c2a97fef9749f3c994a8cde721
matsubara <matsubara@Empress>
2016-06-17 00:02:08 +0900

    アルゴリズムを全面刷新(32分割方式から3分割方式へ)

--- src/mmsort/MasSort.java
+++ src/mmsort/MasSort.java
@@ -1,10 +1,15 @@
 /*
- * MasSort
- * Faster version of the merge sort.
+ * MasSort (Version 2)
+ *
+ * 2016/06
+ * 以前のバージョンのMasSortは配列を32個ほどの区画にわけマージしていたが、
+ * コードが複雑になりすぎて速度が思ったほど上がらなかったため、３つの区画に分け、
+ * ３つの区画の大小関係を STATE_XXX のような整数型変数１つで管理する方式に
+ * 変更した。
  *
  * http://www.mmatsubara.com/developer/sort/
  *
- * Copyright (c) 2015 matsubara masakazu
+ * Copyright (c) 2016 matsubara masakazu
  * Released under the MIT license
  * https://github.com/m-matsubara/sort/blob/master/LICENSE.txt
  */
@@ -13,244 +18,220 @@ package mmsort;
 import java.util.Comparator;
 
 public class MasSort implements ISortAlgorithm {
+	private static final int STATE_123 = 0;
+	private static final int STATE_132 = 1;
+	private static final int STATE_213 = 2;
+	private static final int STATE_231 = 3;
+	private static final int STATE_312 = 4;
+	private static final int STATE_321 = 5;
+
+	private static final int STATE_12 = 6;
+	private static final int STATE_13 = 7;
+	private static final int STATE_21 = 8;
+	private static final int STATE_23 = 9;
+	private static final int STATE_31 = 10;
+	private static final int STATE_32 = 11;
+
+	private static final int STATE_1 = 12;
+	private static final int STATE_2 = 13;
+	private static final int STATE_3 = 14;
+
 	/**
-	 * MasSort
-	 *
-	 * Faster version of the merge sort.
-	 * Merging by dividing the sort object in the many blocks.
-	 * Compared to normal merge sort, it is possible to reduce the memory transfer count,
-	 * it is considered particularly advantageous in slow memory access environment.
-	 *
-	 * 高速化版マージソート
-	 * ソート対象を多くのブロックに分割してマージする。
-	 * 通常のマージソートに比べて、メモリ転送回数を減らすことができるため、メモリアクセスの遅い環境で特に有利と考えられる。
-	 *
-	 * @param arrayFrom sort source / ソート元
-	 * @param arrayTo sort result / ソート結果
-	 * @param from index of first element / ソート対象の開始位置
-	 * @param to index of last element (exclusive) / ソート対象の終了位置 + 1
-	 * @param destIdx sort result destination position /  ソート済み内容配置先インデックス
+	 * マージ処理
+	 * @param array マージ先
+	 * @param pos1 array配列の第１レーンの開始位置
+	 * @param pos2 array配列の第２レーンの開始位置
+	 * @param pos3 array配列の第３レーンの開始位置
+	 * @param to ソート対象の終了位置（含まない位置）
+	 * @param workArray work area / 作業用一時領域
 	 * @param comparator comparator of array element / 比較器
 	 */
-	public static final <T> void masSort(final T[] arrayFrom, final T[] arrayTo, final int from, final int to, final int destIdx, final Comparator<? super T> comparator)
-	{
-		final int range = to - from;			//	ソート範囲サイズ
-
-		//	ソート対象配列サイズが３以下のときは特別扱い
-		if (range <= 1) {
-			return;
-		}
-		if (range < 100) {
-			MergeSort.mergeSort(arrayFrom, from, to, comparator);
-			System.arraycopy(arrayFrom, from, arrayTo, destIdx, to - from);
-			return;
-		}
-
-		int blockCount = 32;							//	通常のマージソートでは２つのブロックに分けるが、masSortではより多くのブロックに分けてマージする。
-		int[] blockStart = new int[blockCount];			//	各ブロックの先頭位置（arrayへの添え字）を保持
-		int[] blockEnd = new int[blockCount];			//	各ブロックの最終位置 + 1（arrayへの添え字）を保持
-		int[] fromOrderBlocks = new int[blockCount];	//	各ブロックの先頭要素を比較し、小さい値を持つブロック№を保持(0 ～ blockCount-1)
-		@SuppressWarnings("unchecked")
-		T[]   blockStartItemCache = (T[])new Object[blockCount];	//	各ブロックの先頭要素をキャッシュ(CPUのキャッシュヒット率が上がるかなと思って)
-
-		//	ソート対象をブロックに分割・ブロックごとにソート
-		for (int blockIdx = 0; blockIdx < blockCount; blockIdx++) {
-			blockStart[blockIdx] = (int)((long)range * blockIdx / blockCount);
-			blockEnd[blockIdx] = (int)((long)range * (blockIdx + 1) / blockCount);
-			masSort(arrayTo, arrayFrom, destIdx + blockStart[blockIdx], destIdx + blockEnd[blockIdx], from + blockStart[blockIdx], comparator);
-			fromOrderBlocks[blockIdx] = blockIdx;
-		}
-
-		//	各ブロックの先頭要素をキャッシュ(CPUのキャッシュヒット率が上がるかなと思って)
-		for (int blockIdx = 0; blockIdx < blockCount; blockIdx++) {
-			blockStartItemCache[blockIdx] = arrayFrom[from + blockStart[blockIdx]];
+	public static final <T> void merge(final T[] array, int pos1, int pos2, int pos3, final int to, final T[] workArray, final Comparator<? super T> comparator) {
+		int mode;
+		if (comparator.compare(array[pos1], array[pos2]) <= 0) {
+			// array[p1] <= array[p2]
+			if (comparator.compare(array[pos2], array[pos3]) <= 0) {
+				// array[p1] <= array[p2] <= array[p3]
+				mode = STATE_123;
+			} else if (comparator.compare(array[pos1], array[pos3]) <= 0) {
+				// array[p1] <= array[p3] <= array[p2]
+				mode = STATE_132;
+			} else {
+				// array[p3] < array[p1] <= array[p2]
+				mode = STATE_312;
+			}
+		} else {
+			// array[p2] < array[p1]
+			if (comparator.compare(array[pos1], array[pos3]) <= 0) {
+				// array[p2] < array[p1] <= array[p3]
+				mode = STATE_213;
+			} else if (comparator.compare(array[pos2], array[pos3]) <= 0) {
+				// array[p2] <= array[p3] < array[p1]
+				mode = STATE_231;
+			} else {
+				// array[p3] < array[p2] < array[p1]
+				mode = STATE_321;
+			}
 		}
 
-		//	各ブロックの先頭で比較して、小さい値を持つブロック順にブロックをソート → fromOrderBlocks
-		//	（二分探索を使った挿入ソート）
-		for (int fromOrderBlocksIdx = 1; fromOrderBlocksIdx < blockCount; fromOrderBlocksIdx++) {
-			int blockIdx = fromOrderBlocks[fromOrderBlocksIdx];	//	ブロックインデックス（このブロックの最小値は他のブロックの最小値と比較して何番目か調べる）
-			//T key = works[blockStart[blockIdx]];
-			T key = blockStartItemCache[blockIdx];				//	キー値
-			int fromIdx = 0;									//	探索範囲の最小インデックス
-			int toIdx = fromOrderBlocksIdx;						//	探索範囲の最大インデックス + 1
-			int curIdx = fromIdx + (toIdx - fromIdx) / 2;		//	探索範囲の中央インデックス
-			//	二分探索処理
-			while (fromIdx < toIdx) {
-				int curBlockIdx = fromOrderBlocks[curIdx];
-				//int compVal = comparator.compare(key, works[blockStart[curBlockIdx]]);
-				int compVal = comparator.compare(key, blockStartItemCache[curBlockIdx]);
-				if (compVal == 0) {		//	キー値が同じであるなら、ブロック番号の小さい方を先にする。（安定のため）
-					if (blockIdx < curBlockIdx)
-						compVal = -1;
+		System.arraycopy(array, pos1, workArray, 0, pos3 - pos1);
+		int idx = pos1;
+		final int p1to = pos2 - idx;
+		final int p2to = pos3 - idx;
+		final int p3to = to;
+		pos1 = 0;
+		pos2 -= idx;
+
+		ThreeLane:
+		for (; idx < to; idx++) {
+			// 以下のif文のネストは、本来なら switch case で処理するべきだが、if のネストのほうが速かったので、このような書き方にしている。
+			if (mode < STATE_213) {
+				// 0-1
+				array[idx] = workArray[pos1++];
+				if (mode == STATE_123) {	// 0 STATE_123
+					if (pos1 >= p1to) {
+						mode = STATE_23;
+						break ThreeLane;
+					} else if (comparator.compare(workArray[pos1], workArray[pos2]) <= 0)
+						; // モード変更なし
+					else if (comparator.compare(workArray[pos1], array[pos3]) <= 0)
+						mode = STATE_213;
 					else
-						compVal = 1;
-				}
-				if (compVal < 0) {
-					toIdx = curIdx;
-				} else {
-					fromIdx = curIdx + 1;
-				}
-				/* virtual code
-					if (key < blockStartItemCache[curBlockIdx]) then
-					begin
-						toIdx = curIdx;
-					end
-					else if (blockStartItemCache[curBlockIdx] < key) then
-					begin
-						fromIdx = curIdx + 1;
-					end
+						mode = STATE_231;
+				} else { 				// 1 STATE_132
+					if (pos1 >= p1to) {
+						mode = STATE_32;
+						break ThreeLane;
+					} else if (comparator.compare(workArray[pos1], array[pos3]) <= 0)
+						; // モード変更なし
+					else if (comparator.compare(workArray[pos1], workArray[pos2]) <= 0)
+						mode = STATE_312;
 					else
-					begin
-						// case of key == blockStartItemCache[curBlockIdx]
-						if (blockIdx < curBlockIdx)
-							toIdx = curIdx
-						else
-							fromIdx = curIdx + 1;
-					end;
-				 */
-				curIdx = fromIdx + (toIdx - fromIdx) / 2;
-			}
-			//	挿入処理
-			for (int j = fromOrderBlocksIdx - 1; j >= curIdx; j--) {
-				fromOrderBlocks[j + 1] = fromOrderBlocks[j];
-			}
-			fromOrderBlocks[curIdx] = fromOrderBlocksIdx;
-		}
-
-		//	先頭要素でソートしたブロックごとにブロックの末尾が次のブロックの先頭より小さければ、ブロック全体を転送することで高速化
-		//	(ソート済み・逆順ソート済みで効果)
-		int idx = 0;
-
-		int blockTransIdx;
-		for (blockTransIdx = 0; blockTransIdx < blockCount - 1; blockTransIdx++) {
-			int blockIdx = fromOrderBlocks[blockTransIdx];		//	比較の基準のブロック
-			int blockIdx2 = fromOrderBlocks[blockTransIdx + 1];	//	比較の基準の次のブロック
-
-			//	「比較の基準のブロックの末尾」と「比較の基準の次のブロックの先頭」を比較
-			int compVal = comparator.compare(arrayFrom[from + blockEnd[blockIdx] - 1], arrayFrom[from + blockStart[blockIdx2]]);
-			if (compVal == 0) {
-				if (blockIdx < blockIdx2)
-					compVal = -1;
-				else
-					compVal = 1;
-			}
-			/* virtual code
-				if (arrayFrom[from + blockEnd[blockIdx] - 1] < arrayFrom[from + blockStart[blockIdx2]]) then
-				begin
-					toIdx = curIdx;
-				end
-				else if (arrayFrom[from + blockStart[blockIdx2]] < arrayFrom[from + blockEnd[blockIdx] - 1]) then
-				begin
-					fromIdx = curIdx + 1;
-				end
-				else
-				begin
-					// case of arrayFrom[from + blockStart[blockIdx2]] == arrayFrom[from + blockEnd[blockIdx] - 1]
-					if (blockIdx < blockIdx2) then
-						toIdx = curIdx
+						mode = STATE_321;
+				}
+			} else if (mode < STATE_312) {
+				// 2-3
+				array[idx] = workArray[pos2++];
+				if (mode == STATE_213) {	// 2 STATE_213
+					if (pos2 >= p2to) {
+						mode = STATE_13;
+						break ThreeLane;
+					} else if (comparator.compare(workArray[pos2], workArray[pos1]) < 0)
+						; // モード変更なし
+					else if (comparator.compare(workArray[pos2], array[pos3]) <= 0)
+						mode = STATE_123;
 					else
-						fromIdx = curIdx + 1;
-				end;
-			*/
-
-			//	「比較の基準のブロックの末尾」が「比較の基準の次のブロックの先頭」より大きければ高速化ロジックを抜ける
-			if (compVal > 0) {
-				break;
-			}
-			//	比較の基準のブロックは後続のすべてのブロックより小さいのでブロックごと転送してしまう。
-			int rangeBlock = blockEnd[blockIdx] - blockStart[blockIdx];
-			System.arraycopy(arrayFrom, from + blockStart[blockIdx], arrayTo, destIdx + idx, rangeBlock);
-			idx += rangeBlock;
-		}
-		//	最後のブロックのみ残った場合は、比較のブロックがないのでこのブロックも転送する。
-		if (blockTransIdx == blockCount - 1) {
-			int blockIdx = fromOrderBlocks[blockTransIdx];
-			int rangeBlock = blockEnd[blockIdx] - blockStart[blockIdx];
-			System.arraycopy(arrayFrom, from + blockStart[blockIdx], arrayTo, destIdx + idx, rangeBlock);
-			idx += rangeBlock;
-			blockTransIdx++;
-		}
-		//	ブロック転送したブロックを fromOrderBlocks の配列から除去
-		for (int fromBlockIdx = blockTransIdx; fromBlockIdx < blockCount; fromBlockIdx++) {
-			fromOrderBlocks[fromBlockIdx - blockTransIdx] = fromOrderBlocks[fromBlockIdx];
-		}
-		blockCount -= blockTransIdx;
-
-		//	全ブロックをマージ
-		//	各ブロックをキューのように扱っていると考えると理解しやすいかも
-		for (/*idx = 0*/; idx < range; idx++) {
-			//	最小の値を持つブロック（fromOrderBlocks[0]）の先頭要素を取り出し、ソート対象へ先頭から詰めていく
-			int blockIdx = fromOrderBlocks[0];
-			int valueIdx = from + blockStart[blockIdx];
-			T value = arrayFrom[valueIdx];
-			arrayTo[destIdx + idx] = value;
-			//	取り出したブロックの先頭位置をインクリメント
-			blockStart[blockIdx]++;
-
-			if (blockStart[fromOrderBlocks[0]] >= blockEnd[fromOrderBlocks[0]]) {
-				//	ブロックのすべての値がなくなったら、fromOrderBlocksの配列からブロック№を取り除く
-				for (int i = 1; i < blockCount; i++)
-					fromOrderBlocks[i - 1] = fromOrderBlocks[i];
-				blockCount--;
-			}
-			else {
-				//	fromOrderBlocks[0] のブロックは先頭の値を取り出したので次の先頭は大きな値になる。二分検索でfromOrderBlocksのどの位置にすればよいか決定し位置を修正する
-				//	（部分的な二分探索を使った挿入ソート…先頭ブロックの最小値が変わったのでそこだけ適切な順序に並べ替え）
-				blockStartItemCache[blockIdx] = arrayFrom[valueIdx + 1];
-				int fromBlockIdx = fromOrderBlocks[0];			//	fromOrderBlocks[0]の最小値は取り出したので新しい最小値が他のブロックの最小値と比較して何番目か調べる
-				//T key = works[blockStart[fromBlockIdx]];
-				T key = blockStartItemCache[fromBlockIdx];
-				int fromIdx = 1;									//	探索範囲の最小インデックス
-				int toIdx = blockCount;								//	探索範囲の最大インデックス
-				int curIdx = fromIdx + (toIdx - fromIdx) / 2;		//	探索範囲の中央インデックス
-				//	二分探索処理
-				while (fromIdx < toIdx) {
-					int curBlockIdx = fromOrderBlocks[curIdx];
-					//int compVal = comparator.compare(key, works[blockStart[curBlockIdx]]);
-					int compVal = comparator.compare(key, blockStartItemCache[curBlockIdx]);
-					if (compVal == 0) {							//	キー値が同じであるなら、ブロック番号の小さい方を先にする。（安定のため）
-						if (fromBlockIdx < curBlockIdx)
-							compVal = -1;
-						else
-							compVal = 1;
-					}
-					if (compVal < 0) {
-						toIdx = curIdx;
-						curIdx = fromIdx + (toIdx - fromIdx) / 2;
-					} else {
-						fromIdx = curIdx + 1;
-					}
-					/* virtual code
-						if (key < blockStartItemCache[curBlockIdx]) then
-						begin
-							toIdx = curIdx;
-							curIdx = fromIdx + (toIdx - fromIdx) / 2;
-						end
-						else if (blockStartItemCache[curBlockIdx] < key) then
-						begin
-							fromIdx = curIdx + 1;
-						end
-						else
-						begin
-							// case of key == blockStartItemCache[curBlockIdx]
-							if (blockIdx < blockIdx2) then
-							begin
-								toIdx = curIdx;
-								curIdx = fromIdx + (toIdx - fromIdx) / 2;
-							end
-							else
-								fromIdx = curIdx + 1;
-						end;
-					*/
-					curIdx = fromIdx + (toIdx - fromIdx) / 2;
+						mode = STATE_132;
+				} else { // STATE_231	// 3 STATE_231
+					if (pos2 >= p2to) {
+						mode = STATE_31;
+						break ThreeLane;
+					} else if (comparator.compare(workArray[pos2], array[pos3]) <= 0)
+						; // モード変更なし
+					else if (comparator.compare(workArray[pos2], workArray[pos1]) < 0)
+						mode = STATE_321;
+					else
+						mode = STATE_312;
 				}
-				//	挿入処理
-				for (int j = 1; j < curIdx; j++) {
-					fromOrderBlocks[j - 1] = fromOrderBlocks[j];
+			} else {
+				// 4-5
+				array[idx] = array[pos3++];
+				if (mode == STATE_312) {	// 4 STATE_312
+					if (pos3 >= p3to) {
+						mode = STATE_12;
+						break ThreeLane;
+					} else if (comparator.compare(array[pos3], workArray[pos1]) < 0)
+						; // モード変更なし
+					else if (comparator.compare(array[pos3], workArray[pos2]) < 0)
+						mode = STATE_132;
+					else
+						mode = STATE_123;
+				} else { 				// 5 STATE_321
+					if (pos3 >= p3to) {
+						mode = STATE_21;
+						break ThreeLane;
+					} else if (comparator.compare(array[pos3], workArray[pos2]) < 0)
+						; // モード変更なし
+					else if (comparator.compare(array[pos3], workArray[pos1]) < 0)
+						mode = STATE_231;
+					else
+						mode = STATE_213;
 				}
-				fromOrderBlocks[curIdx - 1] = fromBlockIdx;
 			}
+		}
+		idx++;
+
+		TwoLane:
+		for (; idx < to; idx++) {
+			// 以下のif文のネストは、本来なら switch case で処理するべきだが、if のネストのほうが速かったので、このような書き方にしている。
+			if (mode < STATE_21) {
+				if (mode == STATE_12) {	// 6 STATE_12
+					array[idx] = workArray[pos1++];
+					if (pos1 >= p1to) {
+						mode = STATE_2;
+						break TwoLane;
+					} else if (comparator.compare(workArray[pos1], workArray[pos2]) <= 0)
+						; // モード変更なし
+					else
+						mode = STATE_21;
+				} else { 				// 7 STATE_13
+					array[idx] = workArray[pos1++];
+					if (pos1 >= p1to) {
+						mode = STATE_3;
+						break TwoLane;
+					} else if (comparator.compare(workArray[pos1], array[pos3]) <= 0)
+						; // モード変更なし
+					else
+						mode = STATE_31;
+				}
+			} else if (mode < STATE_31) {
+				if (mode == STATE_21) {	// 8 STATE_21
+					array[idx] = workArray[pos2++];
+					if (pos2 >= p2to) {
+						mode = STATE_1;
+						break TwoLane;
+					} else if (comparator.compare(workArray[pos2], workArray[pos1]) < 0)
+						; // モード変更なし
+					else
+						mode = STATE_12;
+				} else { 				// 9 STATE_23
+					array[idx] = workArray[pos2++];
+					if (pos2 >= p2to) {
+						mode = STATE_3;
+						break TwoLane;
+					} else if (comparator.compare(workArray[pos2], array[pos3]) <= 0)
+						; // モード変更なし
+					else
+						mode = STATE_32;
+				}
+			} else {
+				if (mode == STATE_31) {	// 10 STATE_31
+					array[idx] = array[pos3++];
+					if (pos3 >= p3to) {
+						mode = STATE_1;
+						break TwoLane;
+					} else if (comparator.compare(array[pos3], workArray[pos1]) < 0)
+						; // モード変更なし
+					else
+						mode = STATE_13;
+				} else { 				// 11 STATE_32
+					array[idx] = array[pos3++];
+					if (pos3 >= p3to) {
+						mode = STATE_2;
+						break TwoLane;
+					} else if (comparator.compare(array[pos3], workArray[pos2]) < 0)
+						; // モード変更なし
+					else
+						mode = STATE_23;
+				}
+			}
+		}
+		idx++;
+
+		if (mode == STATE_1) {
+			System.arraycopy(workArray, pos1, array, idx, p1to - pos1);
+		} else if (mode == STATE_2) {
+			System.arraycopy(workArray, pos2, array, idx, p2to - pos2);
 		}
 	}
 
@@ -258,21 +239,77 @@ public class MasSort implements ISortAlgorithm {
 	/**
 	 * MasSort
 	 *
-	 * Faster version of the merge sort.
-	 * 高速化版マージソート
+	 * 作業用一時領域はソート対象の範囲サイズの2/3(切り捨て)程度必要
+	 * @param array sort target / ソート対象
+	 * @param from index of first element / ソート対象の開始位置
+	 * @param to index of last element (exclusive) / ソート対象の終了位置 + 1
+	 * @param workArray work area / 作業用一時領域
+	 * @param comparator comparator of array element / 比較器
+	 */
+	public static final <T> void masSort(final T[] array, final int from, final int to, final T[] workArray, final Comparator<? super T> comparator)
+	{
+		final int range = to - from;
+
+		//	ソート対象配列サイズが３以下のときは特別扱い
+		if (range <= 1) {
+			return;
+		} else if (range == 2) {
+			if (comparator.compare(array[from + 1], array[from]) < 0) {
+				T work = array[from];
+				array[from] = array[from + 1];
+				array[from + 1] = work;
+			}
+			return;
+		} else if (range == 3) {
+			if (comparator.compare(array[from + 1], array[from]) < 0) {
+				T work = array[from];
+				array[from] = array[from + 1];
+				array[from + 1] = work;
+			}
+			if (comparator.compare(array[from + 2], array[from + 1]) < 0) {
+				T work = array[from + 1];
+				array[from + 1] = array[from + 2];
+				array[from + 2] = work;
+				if (comparator.compare(array[from + 1], array[from]) < 0) {
+					work = array[from];
+					array[from] = array[from + 1];
+					array[from + 1] = work;
+				}
+			}
+			return;
+		}
+
+		final int gap = range / 3;
+		final int pos1 = from;
+		final int pos2 = pos1 + gap;
+		final int pos3 = pos2 + gap;
+		masSort(array, pos3, to,   workArray, comparator);
+		masSort(array, pos2, pos3, workArray, comparator);
+		masSort(array, from, pos2, workArray, comparator);
+
+		// ソート済み配列の場合の高速化
+		if (comparator.compare(array[pos2 - 1], array[pos2]) <= 0 && comparator.compare(array[pos3 - 1], array[pos3]) <= 0)
+			return;
+
+		// マージ処理は外出しにしたほうが初回実行時に速い(再起で小さい範囲を処理している間にJITコンパイラに処理されて、結果的に高速化できる)
+		merge(array, pos1, pos2, pos3, to, workArray, comparator);
+	}
+
+
+	/**
+	 * MasSort
 	 *
 	 * @param array sort target / ソート対象
 	 * @param from index of first element / ソート対象の開始位置
 	 * @param to index of last element (exclusive) / ソート対象の終了位置 + 1
 	 * @param comparator comparator of array element / 比較器
 	 */
-	public static <T> void masSort(final T[] array, final int from, final int to, final Comparator<? super T> comparator)
+	public static final <T> void masSort(T[] array, int from, int to, Comparator<? super T> comparator)
 	{
-		final int range = to - from;
 		@SuppressWarnings("unchecked")
-		final T[] workArray = (T[])new Object[range];
-		System.arraycopy(array, from, workArray, 0, range);
-		masSort(workArray, array, 0, range, from, comparator);
+		final T[] workArray = (T[])new Object[(to - from) / 3 * 2];
+
+		masSort(array, from, to, workArray, comparator);
 	}
 
 	@Override
