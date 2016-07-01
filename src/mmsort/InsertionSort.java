24f749fc6673dac3c0a520e038885340bddaa4da
matsubara <matsubara@Empress>
2016-07-01 23:59:20 +0900

    不要ロジック削除

--- src/mmsort/InsertionSort.java
+++ src/mmsort/InsertionSort.java
@@ -23,37 +23,6 @@ public class InsertionSort implements ISortAlgorithm {
 	 */
 	public static final <T> void insertionSort(final T[] array, final int from, final int to, final Comparator<? super T> comparator)
 	{
-		final int range = to - from;
-
-		//	ソート対象配列サイズが３以下のときは特別扱い
-		if (range <= 1) {
-			return;
-		} else if (range == 2) {
-			if (comparator.compare(array[from + 1], array[from]) < 0) {
-				T work = array[from];
-				array[from] = array[from + 1];
-				array[from + 1] = work;
-			}
-			return;
-		} else if (range == 3) {
-			if (comparator.compare(array[from + 1], array[from]) < 0) {
-				T work = array[from];
-				array[from] = array[from + 1];
-				array[from + 1] = work;
-			}
-			if (comparator.compare(array[from + 2], array[from + 1]) < 0) {
-				T work = array[from + 1];
-				array[from + 1] = array[from + 2];
-				array[from + 2] = work;
-				if (comparator.compare(array[from + 1], array[from]) < 0) {
-					work = array[from];
-					array[from] = array[from + 1];
-					array[from + 1] = work;
-				}
-			}
-			return;
-		}
-
 		for (int idx = from + 1; idx < to; idx++) {
 			final T value = array[idx];
 			T prevValue = array[idx - 1];
