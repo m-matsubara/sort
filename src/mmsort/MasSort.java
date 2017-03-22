6c77fa5061081b387fcadb3738b373193ab9783d
EMPRESS\matsubara <matsu.no.page@gmail.com>
2017-03-22 23:23:34 +0900

    30未満はInsertion Sortに切り替え

--- src/mmsort/MasSort.java
+++ src/mmsort/MasSort.java
@@ -249,33 +249,8 @@ public class MasSort implements ISortAlgorithm {
 	public static final <T> void masSort(final T[] array, final int from, final int to, final T[] workArray, final Comparator<? super T> comparator)
 	{
 		final int range = to - from;
-
-		//	ソート対象配列サイズが３以下のときは特別扱い
-		if (range <= 1) {
-			return;
-		} else if (range == 2) {
-			if (comparator.compare(array[from + 1], array[from]) < 0) {
-				T work = array[from];
-				array[from] = array[from + 1];
-				array[from + 1] = work;
-			}
-			return;
-		} else if (range == 3) {
-			if (comparator.compare(array[from + 1], array[from]) < 0) {
-				T work = array[from];
-				array[from] = array[from + 1];
-				array[from + 1] = work;
-			}
-			if (comparator.compare(array[from + 2], array[from + 1]) < 0) {
-				T work = array[from + 1];
-				array[from + 1] = array[from + 2];
-				array[from + 2] = work;
-				if (comparator.compare(array[from + 1], array[from]) < 0) {
-					work = array[from];
-					array[from] = array[from + 1];
-					array[from + 1] = work;
-				}
-			}
+		if (range < 30) {
+			InsertionSort.insertionSort(array, from, to, comparator);
 			return;
 		}
 
