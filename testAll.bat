fa7cbd5c1ab88965aacb14848b515d5157562cce
EMPRESS\matsubara <matsu.no.page@gmail.com>
2017-08-27 00:55:24 +0900

    テスト対象の取捨選択

--- testAll.bat
+++ testAll.bat
@@ -8,7 +8,7 @@ rem call test.bat mmsort.CombSort %1
 call test.bat mmsort.ManyPivotSort %1
 rem call test.bat mmsort.ManyPivotSort3W %1
 call test.bat mmsort.MmSort %1
-call test.bat mmsort.QuickSort %1
+rem call test.bat mmsort.QuickSort %1
 call test.bat mmsort.QuickSort3WM5 %1
 call test.bat mmsort.QuickSortM3 %1
 call test.bat mmsort.QuickSortM5 %1
@@ -17,7 +17,7 @@ call test.bat mmsort.QuickSortM5 %1
 rem stable sort
 call test.bat mmsort.ArraysSort %1
 rem call test.bat mmsort.BinInsersionSort %1
-call test.bat mmsort.ImprovedMergeSort %1
+rem call test.bat mmsort.ImprovedMergeSort %1
 rem call test.bat mmsort.InsersionSort %1
 rem call test.bat mmsort.InplaceMergeSort %1
 call test.bat mmsort.MasSort %1
